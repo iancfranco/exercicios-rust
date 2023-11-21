e1caf9918309b7b283c360e7cb20aef023f722ba
iancfranco <124786735+iancfranco@users.noreply.github.com>
2023-11-21 15:48:48 -0300

    Update soma-valores.rs

--- soma-valores.rs
+++ soma-valores.rs
@@ -3,18 +3,18 @@ use std::io;
 fn main() {
     println!("Digite seu primeiro valor:");
     let mut v1 = String::new();
-    io::stdin().read_line(&mut v1).expect("Failed to read input");
-    let v1: i32 = v1.trim().parse().expect("Please type a number!");
+    io::stdin().read_line(&mut v1).expect("Input incorreto.");
+    let v1: i32 = v1.trim().parse().expect("O valor não é numérico.");
 
     println!("Digite seu segundo valor:");
     let mut v2 = String::new();
-    io::stdin().read_line(&mut v2).expect("Failed to read input");
-    let v2: i32 = v2.trim().parse().expect("Please type a number!");
+    io::stdin().read_line(&mut v2).expect("Input incorreto.");
+    let v2: i32 = v2.trim().parse().expect("O valor não é numérico.");
 
     println!("Digite seu terceiro valor:");
     let mut v3 = String::new();
-    io::stdin().read_line(&mut v3).expect("Failed to read input");
-    let v3: i32 = v3.trim().parse().expect("Please type a number!");
+    io::stdin().read_line(&mut v3).expect("Input incorreto.");
+    let v3: i32 = v3.trim().parse().expect("O valor não é numérico.");
 
     println!("A soma entre o primeiro e o segundo é:");
     println!("{}", v1 + v2);
