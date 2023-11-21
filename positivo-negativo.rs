59ebf4ef83e6a5716792ad1e79e19b132521b2a4
iancfranco <124786735+iancfranco@users.noreply.github.com>
2023-11-21 15:47:28 -0300

    Update positivo-negativo.rs

--- positivo-negativo.rs
+++ positivo-negativo.rs
@@ -3,8 +3,8 @@ use std::io;
 fn main() {
     println!("Digite seu primeiro valor:");
     let mut v1 = String::new();
-    io::stdin().read_line(&mut v1).expect("Failed to read input");
-    let v1: i32 = v1.trim().parse().expect("Please type a number!");
+    io::stdin().read_line(&mut v1).expect("Input incorreto.");
+    let v1: i32 = v1.trim().parse().expect("O valor não é numérico.");
     if v1 > 0 {
         println!("O valor {} é positivo.", v1);
     } else if v1 == 0 {
